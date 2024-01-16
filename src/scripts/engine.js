d03f03b339cd1b1c993646ea23a88390fb96c6f4
Dhiego Henrique Balthazar de Sousa <dhiegobalthazar@gmail.com>
2024-01-16 20:09:10 -0300

    feat: piano tocando com as teclas do teclado

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,13 +1,25 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const mappedKeys = [];
 
 let audio = new Audio("src/audios/a.wav");
 
 const playTune = (key) => {
     audio.src = `src/audios/${key}.wav`;
     audio.play();
-
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(()=>{
+        clickedKey.classList.remove("active");
+    }, 150)
 }
- 
+
+document.addEventListener("keydown", (e) => {
+    if(mappedKeys.includes(e.key)){
+        playTune(e.key);
+    }
+});
+
 pianoKeys.forEach((key) => {
     key.addEventListener("click", () => playTune(key.dataset.key));
+    mappedKeys.push(key.dataset.key);
 });
